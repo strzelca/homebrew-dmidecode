a574ed13a89ffb60ed4cd410a48aeb8ba4be858b
Ryan Armstrong <ryan@cavaliercoder.com>
2017-06-07 21:45:06 +0800

    Patched dmidecode
    Includes fix for https://github.com/cavaliercoder/dmidecode-osx/issues/3

--- Formula/dmidecode.rb
+++ Formula/dmidecode.rb
@@ -1,9 +1,9 @@
 class Dmidecode < Formula
   desc "Dmidecode reports information about your system's hardware."
   homepage "https://github.com/cavaliercoder/dmidecode-osx"
-  url "https://github.com/cavaliercoder/dmidecode-osx/archive/dmidecode-osx-3-0.tar.gz"
+  url "https://github.com/cavaliercoder/dmidecode-osx/archive/v3.0.tar.gz"
   version "3.0"
-  sha256 "676eb69956b836759f2477dc5b71645bf88ec323512edc144d042ee7d1d78bb8"
+  sha256 "f195cd104ddd2f2d83046f4ee96313305fc06cd82fbdecf0ef9d19da0d9c0886"
 
   def install
     system "make", "dmidecode"
